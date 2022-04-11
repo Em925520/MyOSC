3d902714a4f1d57f3183060633c19186b0e2f814
Emily Xie <xiee178@newschool.edu>
2022-04-11 13:18:39 -0400

    writing down dragon code concept and continue to work on it

--- Assets/Dragon_Send.cs
+++ Assets/Dragon_Send.cs
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020 ExT (V.Sigalkin) */
+﻿/* Copyright (c) 2020 ExT (V.Sigalkin) */
 
 using UnityEngine;
 
@@ -19,33 +19,51 @@ namespace extOSC.Examples
 
 		protected virtual void Start()
 		{
-			//sending dragon's movement by aruino
-			//ex press a button in arduio then send dragon movement (playermovement)
-			var OSCmessage = new OSCMessage(Address);//creates a new message
-													 //check if the position of the dragon is changed then send the float data of the position to console
-			float Dragon_position = transform.localPosition.x;
-			
-			transform.localScale = new Vector3(floatData0mapped, floatData0mapped, floatData0mapped);
-
-
-
-
-
-
-
-
-
-
+			//每次update传一下position        那个vector3 event接收
 			/*
-			var OSCmessage = new OSCMessage(Address);//creates a new message and
-			float currentSize = transform.localScale.x;
-			// the line above : checking if the scale of the object is changed then send the float data to console
-			OSCmessage.AddValue(OSCValue.Float(currentSize));
+			 *  sending dragon's movement by arudino through osc
+			 *  
+			 *  1. write down dragon controller script with the basic movements in start
+			 *  2. send dragon (player)'s position in update whenever player moves
+			 *  3. send dragon's data on pressing a button to blow fire 
+			 *  (if statement that 0 is not blowing fire and 1 is blowing fire once a button is pressed
+			 *  4. if fire collides with enemy twice, destory(enemy)
+			 * 
+			 * 
+			 * 
+			 * receiving data through osc
+			 * 
+			 * 1. detect if the dragon is moving
+			 * 2. if the dragon is moving in the scene, receive the data of the dragon position
+			 * 3. detect if dragon is blowing fire or not, if the dragon is blowing-- receiving "1", if dragon is not blowing receiving "0"
+			 * 4. receiveing data of the #times the fire is blowing colliding with enemy
+			
+			
+			
+
+			var OSCmessage = new OSCMessage(Address);//creates a new message
+			//check if the position of the dragon is changed then send the float data of the position to console 
+			//sending this data to osc
+			float Dragon_position = transform.localPosition.x;
+			OSCmessage.AddValue(OSCValue.Float(Dragon_position));
+			transform.position = new Vector2(floatData0mapped, floatData0mapped);
+
+
+
+
+
+
+
+			Debug.Log("send OSC + " + OSCmessage);
+			Transmitter.Send(OSCmessage);
+
+		/*
+			
 			OSCmessage.AddValue(OSCValue.Float(3.14159f));
 			//OSCmessage.AddValue(OSCValue.String("Hello, world!"));//adding a value of a string called hello world
 
-			Transmitter.Send(OSCmessage);
-			Debug.Log("send OSC + " + OSCmessage);
+			;
+			
 			*/
 		}
 
