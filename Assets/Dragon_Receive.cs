7df83dba85a31fa83a97c0cb698ab6ccbac88fd4
ShangshangLi <lss1111@outlook.com>
2022-04-27 12:08:49 -0400

    fixinput

--- Assets/Dragon_Receive.cs
+++ Assets/Dragon_Receive.cs
@@ -4,13 +4,12 @@ namespace extOSC.Examples
 {
 	public class Dragon_Receive : MonoBehaviour
 	{
-		// public var num;
 
 		#region Public Vars
 
 		
 		public string Address = "/Dragon_movement";
-		public string num;
+		// public string num;
 		//which osc address it is going to listen to
 		//change this osc adress according to the other script so it matches
 
@@ -28,61 +27,51 @@ namespace extOSC.Examples
 
 		#endregion
 
-		void Update()
-		{
+		// void Update()
+		// {
 			
-		}
-
-		// #endregion
+		// }
 
 
 		#region Private Methods
 
-		private void ReceivedMessage(OSCMessage message)
-		// {Debug.Log(message);
-		
+		private void ReceivedMessage(OSCMessage message)		
 		{	
-			var data = message.Values[0].StringValue;
+			// var data = message.Values[0].StringValue;
 			// Debug.Log("val[0]"+data.Split(new char[]{';'})[0]);
 			// Debug.Log("val[1]"+data.Split(new char[]{';'})[1]);
 			// Debug.Log("val[2]"+data.Split(new char[]{';'})[2]);
 			//num=data.Split(new char[]{','})[0];
 			//var num = data.Split(",")[0];
-			var blow = data[0];
-			Debug.Log("blow： " + blow);
-			var left = data[2];
-			Debug.Log("left: " + left);
-			var right = data[4];
-			Debug.Log("right: " + right);
-			
-			// right=
-			// float scaleInt1 = float.Parse(left.Values[1].StringValue)*10;
-			// transform.position = new Vector2(transform.position.x - left1*2, transform.position.y);
-			// Debug.Log("move left");
-		// 	//unity接受并处理收到的arduino的信息，控制龙往右边移动；'
+			// var blow = data[0];
+			// Debug.Log("blow： " + blow);
+			// var left = data[2];
+			// Debug.Log("left: " + left);
+			// var right = data[4];
+			// Debug.Log("right: " + right);
+
+
+
 
 			
-		// 	string scaleRaw0 = message.Values[0].StringValue;
-		// 	Debug.Log("val[0]: " + scaleRaw0);
-		// 	float scaleInt0 = float.Parse(message.Values[0].StringValue)*10;
-		// 	transform.position = new Vector2(transform.position.x, transform.position.y+scaleInt0);//喷火
+			string scaleRaw0 = message.Values[0].StringValue;
+			Debug.Log("val[0]: " + scaleRaw0);
+			float scaleInt0 = float.Parse(message.Values[0].StringValue);
+			transform.position = new Vector2(transform.position.x, transform.position.y+scaleInt0);//喷火
 
-		// 	string scaleRaw1 = message.Values[1].StringValue;
-		// 	Debug.Log("val[1]: " + scaleRaw1);
-		// 	float scaleInt1 = float.Parse(message.Values[1].StringValue)*10;
-		// 	transform.position = new Vector2(transform.position.x + scaleInt1, transform.position.y);
-		// 	Debug.Log("move right");
-		// 	//unity接受并处理收到的arduino的信息，控制龙往右边移动；
+			string scaleRaw1 = message.Values[1].StringValue;
+			Debug.Log("val[1]: " + scaleRaw1);
+			float scaleInt1 = float.Parse(message.Values[1].StringValue);
+			transform.position = new Vector2(transform.position.x + scaleInt1, transform.position.y);
+			Debug.Log("move right");
+			//unity接受并处理收到的arduino的信息，控制龙往右边移动；
 
-		// 	string scaleRaw2 = message.Values[2].StringValue;
-		// 	Debug.Log("val[2]: " + scaleRaw2);
-		// 	float scaleInt2 = float.Parse(message.Values[2].StringValue)*10;
-		// 	transform.position = new Vector2(transform.position.x - scaleInt2, transform.position.y);
-		// 	Debug.Log("move left");
-
-
+			string scaleRaw2 = message.Values[2].StringValue;
+			Debug.Log("val[2]: " + scaleRaw2);
+			float scaleInt2 = float.Parse(message.Values[2].StringValue);
+			transform.position = new Vector2(transform.position.x - scaleInt2, transform.position.y);
+			Debug.Log("move left");
 		}
-
 		#endregion
 	}
 }
