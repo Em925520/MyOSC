51be86fe9314305f750dc4e0cd50382821281374
ShangshangLi <lss1111@outlook.com>
2022-05-10 22:05:40 -0400

    oscsend

--- Assets/ScoringSystem.cs
+++ Assets/ScoringSystem.cs
@@ -7,33 +7,30 @@ using TMPro;//the new TMP text
 //https://www.youtube.com/watch?time_continue=694&v=D0lx90n0s-4&feature=emb_logo
 //this script is to change text when we collect the object (to control everything
 public class ScoringSystem : MonoBehaviour
-{
-    public GameObject scoreText;
-    public static int theScore;
-
-
+    {
+        public GameObject scoreText;
+        public static int theScore;
 
     void Update()
     {
-
-
-        scoreText.GetComponent<TextMeshProUGUI>().text = "SCORE : " + theScore;
+       scoreText.GetComponent<TextMeshProUGUI>().text = "SCORE : " + theScore;
 
     }
-
+    
 }
-//previous code from youtube just changing text when you collect the object
-/*
-public GameObject scoreText;
-public int theScore;
-public AudioSource collectSound;//sound effects when the player touches the object
 
-void OnTriggerEnter2D(Collider2D other)
-  {
-      collectSound.Play();
-      //before we change the score into 50
-      theScore += 1;
-      //reference to the game object text
-      scoreText.GetComponent<Text>().text = "SCORE : " + theScore;
-      Destroy(gameObject);
-  }*/
+//previous code from youtube just changing text when you collect the object
+    /*
+    public GameObject scoreText;
+    public int theScore;
+    public AudioSource collectSound;//sound effects when the player touches the object
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collectSound.Play();
+        //before we change the score into 50
+        theScore += 1;
+        //reference to the game object text
+        scoreText.GetComponent<Text>().text = "SCORE : " + theScore;
+        Destroy(gameObject);
+    }*/
